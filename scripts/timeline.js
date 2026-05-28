af443a80fbe2b74b7e779a5e833846b112b09747
Kerr Beeldens <kerrbeeldens@outlook.com>
2026-05-28 23:07:38 +0200

    Move keyframe functionality to timeline.js

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -40,3 +40,62 @@ playButtonTimeline.addEventListener("click", () => {
   playButtonTimeline.textContent = isPaused ? "Pause" : "play";
   animationBuilder.togglePlay();
 });
+
+
+// Data in the animationBuilder has changed
+animationBuilder.addEventListener("updateAnimation", () => {
+  buildVisualizer();
+});
+
+function buildVisualizer() {
+  const visualizerContainer = document.querySelector(".timeline-container");
+  visualizerContainer.innerHTML = ""
+
+  // Get the animationData from the animationBuilder
+  const timelineData = animationBuilder.timelineData;
+
+  // Get the data from the json
+  timelineData.animations.forEach((animationData) => {
+    animationData.properties.forEach((propertyData) => {
+
+      // Create elements to create for each functionality a row
+      const row = document.createElement("div");
+      row.classList.add("row");
+
+      const track = document.createElement("div");
+      track.classList.add("track");
+
+      const trackText = document.createElement("p");
+      trackText.classList.add("track-label");
+
+      row.append(trackText, track)
+
+      trackText.innerHTML = `<span>${propertyData.property}</span>`;
+
+      // Create for each keyframe point a point on the row
+      propertyData.keyframes.forEach((keyframe) => {
+        const point = document.createElement("div");
+        point.classList.add("keyframe");
+        point.style.setProperty("--p", keyframe.progress);
+        track.appendChild(point);
+
+        // Create a drag for the keyframe and update the value
+        Draggable.create(point, {
+          // Type of way you can dragg the element on the x axis
+          type: 'x',
+          bounds: track,
+          onDragEnd() {
+            const trackWidth = track.offsetWidth;
+            const pointX = this.x + (trackWidth * keyframe.progress);
+            const toProgress = Math.max(0, Math.min(1, pointX / trackWidth));
+
+            animationBuilder.moveKeyframe("el-1", propertyData.property, keyframe.progress, toProgress);
+          }
+        });
+      });
+
+    // Add the elements to the html
+      visualizerContainer.appendChild(row);
+    });
+  });
+};
