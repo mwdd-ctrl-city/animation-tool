18621d4cc3378d53b9c8b972ff730ff05a2a9148
sabrinaz35 <sj.zuurbier@gmail.com>
2026-06-12 12:54:14 +0200

    add select background

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -10,6 +10,7 @@ const animationControlColor = document.querySelectorAll(".animation-control-colo
 const deleteTextButton = document.querySelector(".delete-text button"); 
 const tracksContainer = document.querySelector(".timeline-container");
 const canvas = document.querySelector(".content-canvas");
+const canvasContainer = document.querySelector("#original-canvas")
 
 const addTextButton = document.querySelector(".add-text-button");
 
@@ -182,6 +183,28 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
+
+
+// Claude: Waarom werkt dit niet?
+canvasContainer.addEventListener('dblclick', (event) =>{
+  let canvasId = null
+
+  animationData.getElements().forEach((el, id) => {
+    // Search for the type canvas whitin the elements
+    if (el.type === "canvas") {
+      canvasId = id
+
+      // When it's clicked then the activeElement becomes the canvas id to animate the background
+      activeElementId = canvasId
+
+    }
+    // If it does'nt exist then return null
+    if (!canvasId) return;
+
+    // Gives the selected id from the canvas 
+    animationData.setSelectedText(canvas, canvasId)
+  }) 
+});
 // -----------------------
 // Input change timeline time
 // -----------------------
